3406f2f600a4e2ed214634657bbb12dc4426a4d8
nIgihozo <n.igihozo@alustudent.com>
2025-06-26 01:00:39 -0700

    Add 1-last_digit.py updated python script

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number >= 0:
-    last_digit = number % 10
-else:
-     last_digit = abs(number) % 10
-     if last_digit != 0:
-         last_digit = -last_digit
+last_digit = number % 10
+if number < 0 and last_digit != 0:
+   last_digit = last_digit - 10
 
 if last_digit > 5:
     message = "and is greater than 5"
